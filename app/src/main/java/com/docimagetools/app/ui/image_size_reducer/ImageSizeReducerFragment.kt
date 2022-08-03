d14b2c15fc709de4c7f9423f7e42809ef389c8f2
Chetan Goyal <harshrock616@gmail.com>
2022-08-03 10:57:07 +0530

    Fixes File Not Found Issue

--- app/src/main/java/com/docimagetools/app/ui/image_size_reducer/ImageSizeReducerFragment.kt
+++ app/src/main/java/com/docimagetools/app/ui/image_size_reducer/ImageSizeReducerFragment.kt
@@ -1,19 +1,26 @@
 package com.docimagetools.app.ui.image_size_reducer
 
+import android.app.Activity.RESULT_OK
+import android.content.ContentResolver
 import android.content.Intent
+import android.database.Cursor
 import android.os.Bundle
 import android.provider.MediaStore
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContentResolverCompat.query
 import androidx.core.net.toFile
+import androidx.core.net.toUri
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.docimagetools.app.databinding.FragmentImageSizeReducerBinding
 import kotlinx.coroutines.*
 import java.io.File
+import java.io.IOException
 
 
 class ImageSizeReducerFragment : Fragment() {
@@ -57,16 +64,70 @@ class ImageSizeReducerFragment : Fragment() {
         return root
     }
 
+//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+//        super.onActivityResult(requestCode, resultCode, data)
+//        if (resultCode == AppCompatActivity.RESULT_OK && requestCode == pickImage) {
+//            binding.selectedImageView.setImageURI(data?.data)
+//            Log.i("", "Initial");
+//
+//
+//            Log.i("",data!!.data. toString());
+//            Log.i("", "Initial");
+//
+//
+//            Log.i("",data!!.data?.toFile()?.path!!);
+//            Log.i("", "Initial");
+//            data!!.data?.toFile();
+//            Log.i("", "Initial");
+//
+////            Log.i("",File(data!!.data?.path));
+//
+//            uiScope.launch(Dispatchers.IO){
+//                //asyncOperation
+//                withContext(Dispatchers.Main){
+//
+//
+//                    compress(data!!.data?.toFile());
+//                }
+//
+//            }
+//        }
+//    }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (resultCode == AppCompatActivity.RESULT_OK && requestCode == pickImage) {
-            binding.selectedImageView.setImageURI(data?.data)
-            uiScope.launch(Dispatchers.IO){
+        if (requestCode == pickImage && resultCode == RESULT_OK) {
+            if (data == null) {
+//                showError("Failed to open picture!")
+                return
+            }
+            try {
+                 val actualImage: File? = data.data?.let {
+                    FileUtil.from(this.requireContext(), it)?.also {
+
+            //                    actualImageView.setImageBitmap(loadBitmap(it))
+            //                    actualSizeTextView.text = String.format("Size : %s", getReadableFileSize(it.length()))
+            //                    clearImage()
+                    }
+                }
+                if (actualImage == null) {
+                    Log.i("", "Image is null")
+                } else {
+                    uiScope.launch(Dispatchers.IO){
                 //asyncOperation
                 withContext(Dispatchers.Main){
-                    compress(data?.data?.toFile()?.absoluteFile)
+
+
+                    compress(actualImage!!);
                 }
 
+            }
+
+                }
+
+            } catch (e: IOException) {
+//                showError("Failed to read picture data!")
+                e.printStackTrace()
             }
         }
     }
@@ -79,7 +140,8 @@ class ImageSizeReducerFragment : Fragment() {
 //            Log.i("", "${yourFile.absoluteFile}")
             val imageSizeReducerViewModel =
                 ViewModelProvider(this).get(ImageSizeReducerViewModel::class.java)
-            imageSizeReducerViewModel.compress(binding.root.context, File(data.path))
+            val compressedImage = imageSizeReducerViewModel.compress(binding.root.context, data)
+            binding.selectedImageView.setImageURI(compressedImage.toUri())
         }
     }
 
--- app/src/main/java/com/docimagetools/app/ui/image_size_reducer/ImageSizeReducerViewModel.kt
+++ app/src/main/java/com/docimagetools/app/ui/image_size_reducer/ImageSizeReducerViewModel.kt
@@ -1,13 +1,25 @@
 package com.docimagetools.app.ui.image_size_reducer
 
 import android.content.Context
+import android.graphics.Bitmap
 import androidx.lifecycle.ViewModel
 import id.zelory.compressor.Compressor
+import id.zelory.compressor.constraint.format
+import id.zelory.compressor.constraint.quality
+import id.zelory.compressor.constraint.resolution
+import id.zelory.compressor.constraint.size
 import java.io.File
 
 class ImageSizeReducerViewModel : ViewModel() {
 
-    suspend fun compress(context: Context, file: File) {
-        val compressedImageFile = Compressor.compress(context, file)
+    suspend fun compress(context: Context, file: File): File {
+        val compressedImageFile = Compressor.compress(context, file) {
+            resolution(1280, 720)
+            quality(80)
+            format(Bitmap.CompressFormat.WEBP)
+            size(1024) // 1 KB
+        }
+        return compressedImageFile
+//        Log.i("","After Compression")
     }
 }
